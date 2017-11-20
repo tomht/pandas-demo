c4eb8b75ffc2342e5cdd4d91df67fb3c232e6e5d
tomht <tom.hewlett.taylor@googlemail.com>
2017-11-20 17:07:14 +0000

    Add matplotlib reference and show plot.

--- demo1.py
+++ demo1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/bikes.csv', sep=';', encoding='latin1', parse_dates=['Date'], dayfirst=True, index_col='Date')
 
@@ -7,3 +8,4 @@ print(df[:3])
 print(df['Berri 1'])
 
 df['Berri 1'].plot()
+plt.show()
