ad8c84e56be879bae78dc858966725378399b5ac
tomht <tom.hewlett.taylor@googlemail.com>
2017-11-26 14:55:05 +0000

    Add comments to demo 1.

--- demo1.py
+++ demo1.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Easily read a CSV with one line of code.
+
+# Various options are supported: different separating characters, encodings, etc.
+
+# Choose a column to be the index of the data.
+
 df = pd.read_csv('data/bikes.csv', sep=';', encoding='latin1', parse_dates=['Date'], dayfirst=True, index_col='Date')
+
+# Select rows or columns.
 
 print(df[:3])
 
 print(df['Berri 1'])
+
+# Easily plot the data.
 
 df['Berri 1'].plot()
 plt.show()
