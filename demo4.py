3da27978f2fe9461b0e33655e5451d0d2808fa63
tomht <tom.hewlett.taylor@googlemail.com>
2017-11-26 14:41:29 +0000

    Add comments to demo 4.

--- demo4.py
+++ demo4.py
@@ -6,13 +6,22 @@ berri_bikes = bikes[['Berri 1']]
 
 # Do people bike more on weekends or on weekdays?
 
+# The index is the date of the bike ride. We can extract the weekday from that.
+
 print(berri_bikes.index)
 
 print(berri_bikes.index.weekday)
 
+# We can set the weekday as an additional column in the data.
+
 berri_bikes['weekday'] = berri_bikes.index.weekday
 
+# Then we can do some aggregation on the weekday column.
+
 weekday_counts = berri_bikes.groupby('weekday').aggregate(sum)
+
+# Replace the index for readability.
+
 weekday_counts.index = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 print(weekday_counts)
