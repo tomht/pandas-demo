398dabce6e90b77f4a28055df4ad8f69f3cb5dbf
tomht <tom.hewlett.taylor@googlemail.com>
2017-11-26 14:57:27 +0000

    Add comments to demo 2.

--- demo2.py
+++ demo2.py
@@ -3,15 +3,16 @@ import matplotlib.pyplot as plt
 
 complaints = pd.read_csv('data/311-service-requests.csv')
 
-print(complaints['Complaint Type'])
-
-print(complaints[:5])
+# Combine selection of rows and columns.
 
 print(complaints['Complaint Type'][:5])
+
+# Select more than one column.
 
 print(complaints[['Complaint Type', 'Borough']][:10])
 
 # What are the top 10 most common complaints?
+
 complaint_counts = complaints['Complaint Type'].value_counts()
 print(complaint_counts)
 
